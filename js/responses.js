2b9e7d18f3b53f864506f6403df0e2ffc58e5522
doctormatterhorn <dmf030@gmail.com>
2025-08-05 22:53:31 -0300

    changed participants with students

--- js/responses.js
+++ js/responses.js
@@ -116,11 +116,19 @@ function renderMissingParticipants(missingParticipantsRaw) {
 
 document.addEventListener("DOMContentLoaded", async () => {
   const fileContent = await loadResponses();
-  const participants = await loadStudents();
-  console.log("Loaded students:", participants);
-  console.log("Loaded responses:", fileContent);
+  const respondents = Object.values(fileContent).map(r => r.name);
   
-  // const respondents = Object.values(fileContent).map(r => r.name);
+  const students = await loadStudents();
+
+  const params = new URLSearchParams(window.location.search);
+  const event = params.get('event');
+  const selectedTurmas = event.split("-")[3].split("+");
+
+
+  console.log("Respondents:", respondents);
+  console.log("Participants:", students);
+  console.log("turmas:", selectedTurmas);
+
   // const missingParticipants = participants.filter(p => !respondents.includes(p));
   
   // renderResponses(fileContent);
